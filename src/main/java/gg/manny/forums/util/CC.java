892756acf4aa4a9f18fa6d0149967e30db305f37
Foraged <emzyyt61@gmail.com>
2021-08-28 02:41:37 +0100

    Added all new changes for API V1

--- src/main/java/gg/manny/forums/util/CC.java
+++ src/main/java/gg/manny/forums/util/CC.java
@@ -6,8 +6,11 @@ public class CC {
 
     public static String convert(String string) {
         switch (string.toLowerCase(Locale.ROOT)) {
-            case "owner": { return "darkred"; }
-            case "admin": { return "red"; }
+            case "owner": { return "#AA0000"; }
+            case "developer": { return "#5555FF"; }
+            case "admin": { return "#FF5555"; }
+            case "mod":
+            case "trial-mod": { return "#00AAAA"; }
             case "default": { return "green"; }
 
             default: return "#000";
