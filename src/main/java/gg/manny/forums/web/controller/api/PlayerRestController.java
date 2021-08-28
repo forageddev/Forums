302cbb3b706afabbeb0595ecdc735ac09d2c89ad
Foraged <emzyyt61@gmail.com>
2021-08-28 02:42:19 +0100

    Added all new changes for API V1

--- src/main/java/gg/manny/forums/web/controller/api/PlayerRestController.java
+++ src/main/java/gg/manny/forums/web/controller/api/PlayerRestController.java
@@ -4,31 +4,20 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.stream.JsonReader;
 import gg.manny.forums.Application;
-import gg.manny.forums.rank.Rank;
 import gg.manny.forums.rank.RankRepository;
 import gg.manny.forums.user.User;
 import gg.manny.forums.user.UserRepository;
 import gg.manny.forums.user.grant.Grant;
 import gg.manny.forums.user.punishment.Punishment;
-import gg.manny.forums.user.punishment.PunishmentType;
 import gg.manny.forums.user.service.UserService;
 import gg.manny.forums.util.MojangUtils;
-import org.apache.commons.lang.StringEscapeUtils;
-import org.bson.json.JsonWriter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-import java.io.StringReader;
 import java.util.Date;
-import java.util.Objects;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadLocalRandom;
 
 @RestController
 public class PlayerRestController {
@@ -38,11 +27,30 @@ public class PlayerRestController {
     @Autowired private RankRepository rankRepository;
 
 
-    @RequestMapping(value = "/api/v1/players/{id}", method = RequestMethod.GET)
-    public String getData(@PathVariable UUID id) {
+    @RequestMapping(value = "/api/v1/player", method = RequestMethod.POST)
+    public String getData(HttpServletRequest request) {
         JsonObject data = new JsonObject();
 
-        User user = userRepository.findById(id).orElse(null);
+
+        User user = null;
+
+        String type = request.getParameter("type");
+        String value = request.getParameter("value");
+        switch (type) {
+            case "UUID": {
+                user = userRepository.findById(UUID.fromString(value)).orElse(null);
+                break;
+            }
+            case "USERNAME": {
+                user = userRepository.findByUsernameIgnoreCase(value);
+                break;
+            }
+            case "EMAIL": {
+                user = userRepository.findByEmail(value);
+                break;
+            }
+        }
+
         data.addProperty("success", user != null);
 
         JsonObject playerData = new JsonObject();
@@ -51,11 +59,13 @@ public class PlayerRestController {
             for (Grant grant : user.getGrants()) grants.add(grant.toJson());
             for (Punishment punishment : user.getPunishments()) punishments.add(punishment.toJson());
 
-            playerData.addProperty("id", id.toString());
+            playerData.addProperty("id", user.getId().toString());
             playerData.addProperty("username", user.getUsername());
-
             playerData.addProperty("email", user.getEmail());
             playerData.addProperty("registered", user.isRegistered());
+            playerData.addProperty("dateJoined", user.getDateJoined().getTime());
+            playerData.addProperty("dateLastSeen", user.getDateLastSeen().getTime());
+
             playerData.addProperty("grants", grants.toString());
             playerData.addProperty("punishments", punishments.toString());
         }
@@ -65,9 +75,10 @@ public class PlayerRestController {
         return Application.GSON.toJson(data);
     }
 
-    @RequestMapping(value = "/api/v1/players/{id}/save", method = RequestMethod.POST)
-    public String saveProfile(@PathVariable UUID id, @RequestBody String d) {
-        JsonObject data = new JsonParser().parse(d).getAsJsonObject().get("data").getAsJsonObject();
+    @RequestMapping(value = "/api/v1/player/save", method = RequestMethod.POST)
+    public String saveProfile(HttpServletRequest request) {
+        JsonObject data = new JsonParser().parse(request.getParameter("data")).getAsJsonObject().get("data").getAsJsonObject();
+        UUID id = UUID.fromString(data.get("id").getAsString());
         User user = userRepository.findById(id).orElse(new User());
         boolean error = false;
 
@@ -83,19 +94,17 @@ public class PlayerRestController {
         }
 
         if (!error) {
-            if (data.has("online")) {
-                user.setOnline(data.get("online").getAsBoolean());
-            }
+            if (data.has("dateLastSeen")) user.setDateLastSeen(new Date(data.get("dateLastSeen").getAsLong()));
 
             if (data.has("grants")) {
                 user.getGrants().clear();
                 for (JsonElement element : new JsonParser().parse(data.get("grants").getAsString()).getAsJsonArray()) {
                     user.addGrant(
-                            new Grant(
-                                    element.getAsJsonObject(),
-                                    id,
-                                    rankRepository.findByName(element.getAsJsonObject().get("group").getAsString())
-                            )
+                        new Grant(
+                            element.getAsJsonObject(),
+                            id,
+                            rankRepository.findByName(element.getAsJsonObject().get("group").getAsString())
+                        )
                     );
                 }
             }
@@ -110,43 +119,8 @@ public class PlayerRestController {
         userService.save(user);
 
 
-
-
         JsonObject response = new JsonObject();
         response.addProperty("success", !error);
-        response.add("values", data);
         return Application.GSON.toJson(response);
-    }
-
-    @RequestMapping(value = "/api/v1/players/{id}/addGrant/{rank}/{reason}", method = RequestMethod.GET)
-    public String addGrant(@PathVariable UUID id, @PathVariable Rank rank, @PathVariable String reason) {
-        User user = userRepository.findById(id).orElse(null);
-
-
-
-        Grant grant = new Grant();
-        grant.setId(UUID.randomUUID());
-        grant.setRank(rank);
-        grant.setReason(reason);
-        grant.setIssuedBy(UUID.fromString("392b3a27-d17f-4a02-be8a-2302726946c1"));
-        grant.setDuration(Long.MAX_VALUE);
-        user.addGrant(grant);
-        userRepository.save(user);
-
-        return "Grant added";
-    }
-
-    @RequestMapping(value = "/api/v1/players/{id}/addPunishment/{type}/{reason}", method = RequestMethod.GET)
-    public String addPunishment(@PathVariable UUID id, @PathVariable String type, @PathVariable String reason) {
-        User user = userRepository.findById(id).orElse(null);
-
-        Punishment punishment = new Punishment();
-        punishment.setType(PunishmentType.valueOf(type));
-        punishment.setReason(reason);
-        punishment.setIssuedBy(UUID.fromString("392b3a27-d17f-4a02-be8a-2302726946c1"));
-        user.addPunishment(punishment);
-        userRepository.save(user);
-
-        return "Punishment added";
     }
 }
